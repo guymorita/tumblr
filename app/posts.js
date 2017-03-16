4ef5341d54374f56da57e3dacfea06ea3b727135
Thang <Togepi@Togepi.local>
2017-03-16 20:09:43 +0700

    thang

--- app/posts.js
+++ app/posts.js
@@ -12,37 +12,36 @@ export default class Posts extends Component {
       super();
       const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
       this.state = {
-        dataSource: ds.cloneWithRows([]),
+        dataSource: ds.cloneWithRows(['row 1','row 2']),
       };
     }
-
-    async getMoviesFromApi() {
-      try {
-        let response = await fetch('https://api.tumblr.com/v2/blog/xkcn.info/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV')
-        let responseJson = await response.json()
-        console.log('responseJson', responseJson)
+    componentDidMount(){
+      this.getMoviesFromApiAsync();
+    }
+    getMoviesFromApiAsync() {
+    return fetch('https://api.tumblr.com/v2/blog/xkcn.info/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        alert(responseJson.response.posts.name);
         this.setState({
-          dataSource: this.state.dataSource.cloneWithRows(responseJson.response.posts)
-        })
-      } catch(err) {
-        console.log(err)
-      }
-    }
-
-    componentDidMount() {
-      console.log('mounting')
-      this.getMoviesFromApi().done()
-    }
+          dataSource: this.state.dataSource.cloneWithRows(responseJson.response.posts),
+        });
+      })
+      .catch((error) => {
+        alert('test2');
+        console.error(error);
+      });
+  }
 
     render() {
       return (
+        <View style={{flex: 1, paddingTop: 22}}>
         <ListView
           dataSource={this.state.dataSource}
-          renderRow={(rowData) => {
-            <Text>{rowData.summary}</Text>
-          }}
+          renderRow={(rowData) => <Text>{rowData.summary}</Text>}
         />
+      </View>
+
       );
     }
-
-}
+  }
