436d785b10dbfc37a9b7b23f7357366576b563bf
Guy Morita <guymorita@gmail.com>
2017-03-16 19:32:11 +0700

    add api call

--- app/posts.js
+++ app/posts.js
@@ -12,15 +12,33 @@ export default class Posts extends Component {
       super();
       const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
       this.state = {
-        dataSource: ds.cloneWithRows(['row 1', 'row 2']),
+        dataSource: ds.cloneWithRows([]),
       };
+    }
+
+    async getMoviesFromApi() {
+      try {
+        let response = await fetch('https://api.tumblr.com/v2/blog/xkcn.info/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV')
+        let responseJson = await response.json()
+        this.setState({
+          dataSource: this.state.dataSource.cloneWithRows(responseJson.response.posts)
+        })
+      } catch(err) {
+        console.log(err)
+      }
+    }
+
+    componentDidMount() {
+      this.getMoviesFromApi()
     }
 
     render() {
       return (
         <ListView
           dataSource={this.state.dataSource}
-          renderRow={(rowData) => <Text>{rowData}</Text>}
+          renderRow={(rowData) => {
+            <Text>{rowData}</Text>
+          }
         />
       );
     }
